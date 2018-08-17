de4db34acb720b8e31cfc97651ca39a105895a03
Lysenia <lesialysiak@gmail.com>
2018-08-16 23:42:36 -0400

    properties file change

--- src/test/java/com/app/step_definitions/CommonFunctionalityStepDefs.java
+++ src/test/java/com/app/step_definitions/CommonFunctionalityStepDefs.java
@@ -1,5 +1,114 @@
 package com.app.step_definitions;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class CommonFunctionalityStepDefs {
+	
+	@Given("the teacher is on home page")
+	public void the_teacher_is_on_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("the teacher goes to hunt")
+	public void the_teacher_goes_to_hunt() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("the teacher should be able to reserve room from {int} minutes to {int} hours")
+	public void the_teacher_should_be_able_to_reserve_room_from_minutes_to_hours(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("the tech-lead is on home page")
+	public void the_tech_lead_is_on_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("the tech-lead goes to hunt")
+	public void the_tech_lead_goes_to_hunt() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("the tech-lead should be able to reserve room from {int} minutes to {int} hours")
+	public void the_tech_lead_should_be_able_to_reserve_room_from_minutes_to_hours(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("reservation options should be given in a format:from $ time to $time")
+	public void reservation_options_should_be_given_in_a_format_from_$_time_to_$time() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("the teacher is on hunt page")
+	public void the_teacher_is_on_hunt_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("the teacher enters valid date")
+	public void the_teacher_enters_valid_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("reservation window for the day should be from {int}am to {int} pm")
+	public void reservation_window_for_the_day_should_be_from_am_to_pm(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("the tech-lead is on hunt page")
+	public void the_tech_lead_is_on_hunt_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("the tech-lead enters valid date")
+	public void the_tech_lead_enters_valid_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("the teacher is on the hunt page")
+	public void the_teacher_is_on_the_hunt_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("the teacher goes to date")
+	public void the_teacher_goes_to_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("the calendar should be displayed")
+	public void the_calendar_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("the tech-lead is on the hunt page")
+	public void the_tech_lead_is_on_the_hunt_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("the tech-lead goes to date")
+	public void the_tech_lead_goes_to_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
 
 }
